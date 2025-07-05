048da01ba92f92b003883650ee9a332c289dc078
Fahira <fahira.ash@gmail.com>
2025-07-05 21:55:47 +0500

    prepared for the development

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,20 +5,24 @@ import dotenv from "dotenv"
 import notesRoutes from "./routes/notesRoutes.js"
 import connectDB from "./config/db.js"
 import rateLimiter from "./middleware/rateLimiter.js";
-
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5001;
+const __dirname = path.resolve(); // Get the current directory name
 
 //middleware
-app.use(cors(
-    {
-        origin: "http://localhost:5173",
-        credentials: true,
-    }
-));
+if (process.env.NODE_ENV !== "production") {
+    app.use(cors(
+        {
+            origin: "http://localhost:5173",
+            credentials: true,
+        }
+    ));
+}
+
 app.use(express.json()); // to parse JSON bodies
 app.use(rateLimiter);
 
@@ -30,6 +34,14 @@ app.use(rateLimiter);
 // })
 
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get(/^\/(?!api).*/, (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    })
+}
 
 connectDB(() => {
     app.listen(port, () => {
